e0e9ca8a6c61175df48c69b093796445a6d1c1ff
Erik Laki <erik.laki@student.tuke.sk>
2025-01-14 14:54:22 +0100

    test

--- pipeline-project/terraform/modules/storage/main.tf
+++ pipeline-project/terraform/modules/storage/main.tf
@@ -3,11 +3,6 @@
 resource "google_storage_bucket" "data_pipeline_bucket" {
   name     = var.bucket_name
   location = var.bucket_location
-  labels = {
-    environment = "development"
-    project     = "data-pipeline"
-    managed_by  = "terraform"
-  }
   
   versioning {
     enabled = true
