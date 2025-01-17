705af83194a327726a4acd0be532bba3145dcb67
Erik Laki <erik.laki@student.tuke.sk>
2025-01-17 20:13:17 +0100

    integrated flow into our infrastracture

---
+++ pipeline-project/src/processing/flows/deploy.py
@@ -0,0 +1,87 @@
+import os
+from prefect import serve
+from prefect_kubernetes.jobs import KubernetesJob
+from taxi_data_flow import NY_Taxi_Data_Flow
+
+# Set environment variable for deployment creation
+os.environ["PREFECT_API_URL"] = "http://prefect-server:4200/api"
+
+# Configure the Kubernetes job
+k8s_job = KubernetesJob(
+    v1_job={
+        "apiVersion": "batch/v1",
+        "kind": "Job",
+        "metadata": {
+            "namespace": "data-pipeline"
+        },
+        "spec": {
+            "template": {
+                "spec": {
+                    "containers": [{
+                        "name": "flow",
+                        "image": "gcr.io/teak-gamma-442315-f8/taxi-flow:latest",
+                        "imagePullPolicy": "Always",
+                        "env": [
+                            {"name": "PREFECT_API_URL", "value": "http://prefect-server:4200/api"},
+                            {"name": "DB_HOST", "value": "cloudsql-proxy"},
+                            {"name": "DB_PORT", "value": "5432"},
+                            {"name": "GCS_BUCKET", "value": "data-pipeline-parquet-teak-gamma-442315-f8"},
+                            {"name": "GOOGLE_APPLICATION_CREDENTIALS", "value": "/var/secrets/google/key.json"},
+                            {
+                                "name": "DB_NAME",
+                                "valueFrom": {
+                                    "secretKeyRef": {
+                                        "name": "cloudsql-credentials",
+                                        "key": "database"
+                                    }
+                                }
+                            },
+                            {
+                                "name": "DB_USER",
+                                "valueFrom": {
+                                    "secretKeyRef": {
+                                        "name": "cloudsql-credentials",
+                                        "key": "username"
+                                    }
+                                }
+                            },
+                            {
+                                "name": "DB_PASSWORD",
+                                "valueFrom": {
+                                    "secretKeyRef": {
+                                        "name": "cloudsql-credentials",
+                                        "key": "password"
+                                    }
+                                }
+                            }
+                        ],
+                        "volumeMounts": [{
+                            "name": "gcp-key",
+                            "mountPath": "/var/secrets/google",
+                            "readOnly": True
+                        }]
+                    }],
+                    "volumes": [{
+                        "name": "gcp-key",
+                        "secret": {
+                            "secretName": "gcp-sa-key"
+                        }
+                    }],
+                    "serviceAccountName": "default",
+                    "restartPolicy": "Never"
+                }
+            },
+            "backoffLimit": 0
+        }
+    }
+)
+
+# Create deployment with job configuration
+deployment = NY_Taxi_Data_Flow.to_deployment(
+    name="taxi-data-flow",
+    work_pool_name="k8s-pool",
+    work_queue_name="default"
+)
+
+if __name__ == "__main__":
+    serve(deployment)
