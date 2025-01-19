21c8b01ffa5ea4d3c4394f3ff4c7536f5c8c0d35
Erik Laki <erik.laki@student.tuke.sk>
2025-01-19 18:10:58 +0100

    debugging another try

--- pipeline-project/src/processing/flows/deploy.py
+++ pipeline-project/src/processing/flows/deploy.py
@@ -1,36 +1,15 @@
-# import os
-# from prefect import flow
-# from prefect_kubernetes.jobs import KubernetesJob
-# from taxi_data_flow import NY_Taxi_Data_Flow
-
-# # Set environment variable for deployment creation
-# os.environ["PREFECT_API_URL"] = "http://prefect-server:4200/api"
-
-# # Create deployment
-# deployment = NY_Taxi_Data_Flow.to_deployment(
-#     name="taxi-data-flow",
-#     work_pool_name="k8s-pool",
-#     work_queue_name="default"
-# )
-
-# if __name__ == "__main__":
-#     print("Registering flow deployment with Prefect server...")
-#     deployment.apply()  # Just register and exit
-#     print("Flow deployment registered successfully!")
-import os
 from prefect import serve
-from prefect_kubernetes.jobs import KubernetesJob
+from prefect.deployments import Deployment
 from taxi_data_flow import NY_Taxi_Data_Flow
 
-# Set environment variable for deployment creation
-os.environ["PREFECT_API_URL"] = "http://prefect-server:4200/api"
-
-# Create deployment with job configuration
-deployment = NY_Taxi_Data_Flow.to_deployment(
+# Create deployment
+deployment = Deployment.build_from_flow(
+    flow=NY_Taxi_Data_Flow,
     name="taxi-data-flow",
     work_pool_name="k8s-pool",
     work_queue_name="default"
 )
 
 if __name__ == "__main__":
-    serve(deployment)
+    deployment.apply()
+    print("Deployment registered successfully!")
